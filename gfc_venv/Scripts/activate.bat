d2f18805c4e92c42db0f55af31080438693edc91
Eddie Biggs <edbiggs12@gmail.com>
2024-01-22 15:09:03 -0500

    Updated requirements, added pg_config to path

--- gfc_venv/Scripts/activate.bat
+++ gfc_venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\edbig\Documents\Coding Temple\Week12\Day3\GFC Flask Test\gfc_venv
+set VIRTUAL_ENV=C:\Users\edbig\Documents\Projects\GFC-Final\gfc_venv
 
 if not defined PROMPT set PROMPT=$P$G
 
